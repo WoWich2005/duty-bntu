41c03e88ca9431f2e600b963c15560c7f89d43fc
Vladimir2005 <Lemyashevich2005@gmail.com>
2023-09-10 21:07:46 +0300

    Customize header

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,21 @@
 $(function() {
+	let header = $('.header');
+	let headerHeight = header.height();
+	
+	$(window).on('scroll', function() {
+		if($(this).scrollTop() > 1) {
+			header.addClass('header--fixed');
+			$('body').css({
+				'paddingTop': headerHeight + 'px'
+			});
+		} else {
+			header.removeClass('header--fixed');
+			$('body').css({
+				'paddingTop': 0
+			})
+		}
+	});
+
 	let daysToWatch = 2;
 
 	let checkboxes = $('.timetable > tbody tr > td > .form-check > .form-check-input');
